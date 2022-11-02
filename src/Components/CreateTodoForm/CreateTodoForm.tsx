241a417fa223656d7da185603499d39e1da5591e
Yaroslav Taranenko <yarikT94@gmail.com>
2022-11-02 19:07:27 +0200

    fix(CreateTodoForm.tsx): api headers: now todo add feature is working

--- src/Components/CreateTodoForm/CreateTodoForm.tsx
+++ src/Components/CreateTodoForm/CreateTodoForm.tsx
@@ -15,6 +15,9 @@ function CreateTodoForm() {
         try {
             let res = await fetch(api.save, {
                 method: "POST",
+                headers: {
+                    "Content-Type": "application/json",
+                },
                 body: JSON.stringify({
                     title: title,
                     text: text,
